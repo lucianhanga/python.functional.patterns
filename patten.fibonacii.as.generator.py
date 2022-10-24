3868ea1c35eae163675602dafc94a09382fc5022
lucian <lucianhanga@gmx.net>
2022-10-24 07:49:17 +0200

    update the fibo

--- patten.fibonacii.as.generator.py
+++ patten.fibonacii.as.generator.py
@@ -1,31 +1,65 @@
 
+from email.generator import Generator
 from typing import Iterable, Iterator, Optional
-    
+
+#
+# FibGen - First Class Function Generator
+#
+#    
 class FibGen:
-    def __init__(self, end:Optional[int] = None) -> None:
-        self.n_2 = None
-        self.n_1 = None
-        self.end = end
+    def __init__(self, elements:Optional[int] = None) -> None:
+        self.n_2 = 1
+        self.n_1 = 1
+        self.counter = 0 # need it to know when to return 1,1
+        self.elements = elements
     def __iter__(self) -> Iterator[int]:
         return self
     def __next__(self) -> Iterator[int]:
-        if not self.n_2: 
-            self.n_2 = 1
-            return self.n_2 
-        if not self.n_1:
-            self.n_1 = 1
-            return self.n_1
+        if self.elements and self.counter >= self.elements:
+            raise StopIteration
+        self.counter += 1
+        if self.counter <= 2:
+            return 1
         res = self.n_1 + self.n_2
         self.n_2 = self.n_1
         self.n_1 = res
-        if self.end and res > self.end:
-            raise StopIteration
         return res
 
 # unlimited fibgen 
 fibgen = FibGen()
-print(tuple(  next(fibgen) for _ in range(0,100)))
+print(tuple(  next(fibgen) for _ in range(0,10)))
 
 # limited fibgen. stop when max value reached
-fibgen = FibGen(1000)
-print(tuple(fibgen))
+fibgen = FibGen(10)
+print(tuple(fibgen)) 
+
+
+#
+# fibonacii_gen - generator
+#
+def fibonacii_gen(elements:Optional[int]=None) -> Iterator[int]:
+    yield 1
+    if elements is not None : elements -= 1
+    if elements is not None and elements <= 0: return 
+    yield 1
+    if elements is not None : elements -= 1
+    if elements is not None and elements <= 0: return 
+    n_2 = 1
+    n_1 = 1
+    while True:
+        res = n_2 + n_1
+        yield res
+        if elements is not None: elements -= 1
+        if elements is not None and elements <= 0: return
+        n_2 = n_1
+        n_1 = res
+    return 
+
+# unlimited fibonacii_gen
+fibgen2 = fibonacii_gen()
+print(tuple( next(fibgen2) for _ in range(0,10)))
+
+# limited fibgen. stop when max value reached
+fibgen2 = fibonacii_gen(10)
+print(tuple(fibgen2)) 
+
