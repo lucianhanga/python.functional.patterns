348d3da2180637552208427d043ae9ff6e623c1d
lucian <lucianhanga@gmx.net>
2022-10-24 20:32:57 +0200

    add the decorator pattern to git

---
+++ pattern.high.order.decorator.py
@@ -0,0 +1,34 @@
+#
+# define a high-order function which behaves like a decorator
+#
+# this example checks the paramter. if the parameter is None it returns
+# imediatelly None. If the parameter is not None call the original function
+#
+#
+
+from typing import Callable, Optional, TypeVar, Any, cast
+from functools import wraps
+import logging
+
+F = TypeVar('F' , bound=Callable[..., Any])
+
+def nullsafe ( func: F ) -> F:    
+    @wraps(func) # get the __name__ and __doc__ from the func
+    def nullsafe_wrapper( arg: Optional[Any] ) -> Optional[Any]:
+        return None if arg is None else func(arg)
+    return cast(F, nullsafe_wrapper)
+
+
+# usage example
+# if the decorator is not used the following error will aper:
+#
+#  TypeError: unsupported operand type(s) for *: 'NoneType' and 'NoneType'
+#
+@nullsafe 
+def pow2( arg:Any ) -> Any:
+    return arg*arg
+
+print(tuple(map(pow2, (1,2,None,3,4,None,5))))
+
+
+
